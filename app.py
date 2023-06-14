efc5754306d1e0ebaede87d0614c9734bf9240fe
Ömer Faruk Eren <101640251+hustledeveloper@users.noreply.github.com>
2023-06-14 10:50:13 +0300

    success
    devel volumes düzeltildi, redisten otomatik db ye yazdırma eklendi

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 from flask import Flask, request, jsonify
-from rq import Queue
-from redis import Redis
+import psycopg2
 import logging
 from utils import process_user
 
@@ -8,15 +7,21 @@ from utils import process_user
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
 app = Flask(__name__)
-redis_conn = Redis(host='redis', port=6379)
-queue = Queue(connection=redis_conn)
 
 @app.route('/users', methods=['POST'])
 def enqueue_user():
-    user = request.get_json()
-    queue.enqueue(process_user, user)
-    logging.info('User enqueued successfully')
-    return jsonify({'message': 'User enqueued successfully'}), 200
+    user_list = request.get_json(force=True)
+    if isinstance(user_list, list):
+        for user in user_list:
+            if isinstance(user, dict):
+                process_user(user)
+            else:
+                logging.error('Invalid user format')
+        logging.info('Users enqueued and processed successfully')
+        return jsonify({'message': 'Users enqueued and processed successfully'}), 200
+    else:
+        logging.error('Invalid user list format')
+        return jsonify({'error': 'Invalid user list format'}), 400
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
